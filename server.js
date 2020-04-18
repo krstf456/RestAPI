2762332f843258122715a32edf0ced8b2c3c5ee1
Kristóf <krstf456@gmail.com>
2020-04-18 13:16:18 +0200

    validation

--- server.js
+++ server.js
@@ -13,13 +13,29 @@ const products = [
      }, {
         id: 3,
         name: 'iPad',
-        price: ''
+        price: '3500'
      }
 ]
 
 app.use(express.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/products', (req, res) => {
     res.json(products)
 })
+
+
+
+app.post('/products', (res, req) => {
+    const product = {
+        id: req.body.length + 1,
+        name: req.body.name,
+        price: req.body.price
+    }
+    products.push(product)
+    res.json(product)
+})
+
+app.put()
+
+app.listen(3000, 'localhost', () => console.log('Server is running at port 3000.'))
